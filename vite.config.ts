fd88885ecd24d2e3782a42ac26dca0ac488cc94a
Dan Andrei <ghinea.danandrei@gmail.com>
2024-08-13 18:27:14 +0300

    chore: :sparkles: Add Redux package
    Add the Redux and Redux React as dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sass from 'sass';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true
+  },
   css: {
     preprocessorOptions: {
       scss: {
